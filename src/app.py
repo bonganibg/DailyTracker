7974d552e2040b47a50fda05934e71a8e76771cf
Bonganibg <bongani.gumbo@yahoo.com>
2024-02-21 15:50:35 +0200

    added toggl route

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from src.routes.daily_tracker_api import user_route, expected_hours_route, hours_worked_route, hours_off_route, overtime_route
-
+from src.routes.toggl_api import toggl_route
 
 # Add the routers 
 router = APIRouter()
@@ -9,3 +9,6 @@ router.include_router(expected_hours_route.router)
 router.include_router(hours_worked_route.router)
 router.include_router(hours_off_route.router)
 router.include_router(overtime_route.router)
+
+# Toggl
+router.include_router(toggl_route.router)
